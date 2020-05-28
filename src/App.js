54c137adb0fb8bae565777836d56477227d65400
ThangPhan <pcthangk59@gmail.com>
2020-05-28 15:51:58 +0900

    Init suspense

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense, useState } from "react";
+import "./App.css";
+import { fetchProfileData } from "./fakeApi.js";
+
+const initialResource = fetchProfileData(0);
+
+function getNextId(id) {
+  return id === 3 ? 0 : id + 1;
+}
+
+function ProfilePage({ resource }) {
+  return (
+    <Suspense fallback={<h1>Loading profile...</h1>}>
+      <ProfileDetails resource={resource} />
+      <Suspense fallback={<h1>Loading posts...</h1>}>
+        <ProfileTimeline resource={resource} />
+      </Suspense>
+    </Suspense>
+  );
+}
+
+function ProfileDetails({ resource }) {
+  console.log("resource", resource);
+  const user = resource.user.read();
+  return <h1>{user.name}</h1>;
+}
+
+function ProfileTimeline({ resource }) {
+  const posts = resource.posts.read();
+  return (
+    <ul>
+      {posts.map(post => (
+        <li key={post.id}> {post.text}</li>
+      ))}
+    </ul>
+  );
+}
 
 function App() {
+  const [resource, setResource] = useState(initialResource);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button
+        onClick={() => {
+          const nextUserId = getNextId(resource.userId);
+          setResource(fetchProfileData(nextUserId));
+        }}
+      >
+        Next
+      </button>
+      <ProfilePage resource={resource} />
+    </>
   );
 }
 
